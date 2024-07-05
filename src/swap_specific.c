c4f9f4b68a6850b68060bf0328737323c092e32c
Silvestri Ndoj <tarazhsilvi@gmail.com>
2024-07-06 01:18:26 +0200

    a

--- src/swap_specific.c
+++ src/swap_specific.c
@@ -6,7 +6,7 @@
 /*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 16:22:45 by silndoj           #+#    #+#             */
-/*   Updated: 2024/07/06 00:25:24 by silndoj          ###   ########.fr       */
+/*   Updated: 2024/07/06 01:14:41 by silndoj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,23 +44,21 @@ void	sort_5(int *stack_a, int *stack_b, int *len_a, int *len_b)
 	sort_3(stack_a, *len_a);
 	if (stack_b[1] > stack_b[0])
 		trick_sb(stack_b, *len_b);
-	if (stack_b[0] < stack_a[2])
+	trick_pa(stack_a, stack_b, len_a, len_b);
+	if (stack_a[0] < stack_a[3])
 	{
-		trick_pa(stack_a, stack_b, len_a, len_b);
 		sort_3(stack_a, 2);
 		trick_pa(stack_a, stack_b, len_a, len_b);
 		sort_3(stack_a, 2);
 	}
-	else if (stack_b[0] > stack_a[2] && stack_b[1] > stack_a[2])
+	else if (stack_a[0] > stack_a[2] && stack_b[0] > stack_a[3])
 	{
-		trick_pa(stack_a, stack_b, len_a, len_b);
 		trick_pa(stack_a, stack_b, len_a, len_b);
 		trick_ra(stack_a, *len_a);
 		trick_ra(stack_a, *len_a);
 	}
 	else
 	{
-		trick_pa(stack_a, stack_b, len_a, len_b);
 		trick_ra(stack_a, *len_a);
 		trick_pa(stack_a, stack_b, len_a, len_b);
 		sort_3(stack_a, 2);
